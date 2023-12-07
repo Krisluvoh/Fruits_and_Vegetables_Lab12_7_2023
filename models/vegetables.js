ec2d1ec8fe27299e8bb35814e4e86276faba7b38
Krisluvoh <krisluvohio@gmail.com>
2023-12-07 16:10:00 -0500

    first commit

---
+++ models/vegetables.js
@@ -0,0 +1,29 @@
+const vegetables = [
+    {
+        name:'corn',
+        color: 'yellow',
+        readyToEat: true
+    },
+    {
+        name:'pepper',
+        color: 'green',
+        readyToEat: false
+    },
+    {
+        name:'onion',
+        color: 'white',
+        readyToEat: true
+    },
+    {
+        name:'eggplant',
+        color: 'purple',
+        readyToEat: false
+    },
+    {
+        name:'carrot',
+        color: 'orange',
+        readyToEat: false
+    },
+];
+
+module.exports = vegetables;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fruits = require('./models/fruits.js');
+const vegetables = require('./models/vegetables.js');
 const jsxViewEngine = require('jsx-view-engine');
 
 app.set('view engine', 'jsx');
@@ -28,6 +29,44 @@ app.engine('jsx', jsxViewEngine());
 //     }
 // ];
 
+
+// moving the vegetables to models/vegetables.js in order to have compartmentalized code
+// we are using MVC - models-views-controllers - architecture
+// https://developer.mozilla.org/en-US/docs/Glossary/MVC
+// const vegetables = [
+   // {
+   //     name:'corn',
+    //    color: 'yellow',
+    //    readyToEat: true
+   // },
+   // {
+   //     name:'pepper',
+   //     color: 'green',
+   //     readyToEat: false
+   // },
+   // {
+   //     name:'onion',
+   //     color: 'white',
+   //     readyToEat: true
+   // },
+   // {
+   //     name:'eggplant',
+   //     color: 'purple',
+   //     readyToEat: false
+  //  },
+   // {
+    //    name:'carrot',
+    //    color: 'orange',
+    //    readyToEat: false
+  //  },
+//];
+
+
+
+
+
+
+
 // ================ Middleware ================
 //
 app.use((req, res, next) => {
@@ -52,22 +91,32 @@ app.use(express.urlencoded({extended:false}));
 // S - Show     GET         READ - display a specific element
 
 app.get('/', (req, res) => {
-    res.send('this is my fruits root route');
+    res.send('this is my fruits and vegetables root route');
 });
 
+// I - INDEX - dsiplays a list of all vegetabless
+app.get('/vegetables/', (req, res) => {
+    // res.send(fruits);
+    // res.send(vegetables);
+    res.render('vegetables/Index', {vegetables: vegetables});
+});
 // I - INDEX - dsiplays a list of all fruits
 app.get('/fruits/', (req, res) => {
     // res.send(fruits);
-    res.render('Index', {fruits: fruits});
+    // res.send(vegetables);
+    res.render('fruits/Index', {fruits: fruits});
+   
 });
-
 
 // N - NEW - allows a user to input a new fruit
 app.get('/fruits/new', (req, res) => {
-    res.render('New');
+    res.render('fruits/New');
 });
 
-
+// N - NEW - allows a user to input a new vegetable
+app.get('/vegetables/new', (req, res) => {
+    res.render('vegetables/New');
+});
 // C - CREATE - update our data store
 app.post('/fruits', (req, res) => {
     if(req.body.readyToEat === 'on') { //if checked, req.body.readyToEat is set to 'on'
@@ -82,14 +131,42 @@ app.post('/fruits', (req, res) => {
     res.redirect('/fruits'); // send user back to /fruits
 })
 
+// C - CREATE - update our data store
+app.post('/vegetables', (req, res) => {
+    if(req.body.readyToEat === 'on') { //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true;
+    } else {  //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false;
+    }
+    vegetables.push(req.body);
+    // console.log(fruits);
+    // console.log(req.body)
+    // res.send('data received');
+    res.redirect('/vegetables'); // send user back to /vegetables
+})
+
+
 // S - SHOW - show route displays details of an individual fruit
 app.get('/fruits/:indexOfFruitsArray', (req, res) => {
     // res.send(fruits[req.params.indexOfFruitsArray]);
-    res.render('Show', {// second parameter must be an object
+    res.render('fruits/Show', {// second parameter must be an object
         fruit: fruits[req.params.indexOfFruitsArray]
     });
 })
 
 app.listen(3000, () => {
+    console.log('listening');
+});
+
+
+// S - SHOW - show route displays details of an individual vegetable
+app.get('/vegetables/:indexOfVegetablesArray', (req, res) => {
+    // res.send(vegetables[req.params.indexOfVegetablesArray]);
+    res.render('vegetables/Show', {// second parameter must be an object
+        vegetable: vegetables[req.params.indexOfVegetablesArray]
+    });
+})
+
+app.listen(3001, () => {
     console.log('listening');
 });
